217b9c8e5807cba9f6d27df3d38697c6878c33a6
Shri2703 <poornashri2703@gamil.com>
2024-03-31 22:17:54 +0530

    task

--- CRUD Operations/index.js
+++ CRUD Operations/index.js
@@ -13,8 +13,6 @@ app.get("/", (req, res) => {
   res.send("Hello from Node API Server Updated");
 });
 
-
-
 // app.use("/api/products", productRoute);
 
 mogoose.connect("mongodb+srv://PoornashriAPI:1yR6TvUvpbAfyCqN@cluster0.gk6ac7u.mongodb.net/Node-API?retryWrites=true&w=majority")
